2487a4dc165d07c58c81e083808367766e653a6c
camilord <camilo3rd@gmail.com>
2014-05-02 16:58:40 +1200

    some updates
    some updates

--- Parent/application/controllers/ajax.php
+++ Parent/application/controllers/ajax.php
@@ -124,11 +124,10 @@ class Ajax extends CI_Controller {
                                     Action <span class="caret"></span>
                                 </button>
                                 <ul class="dropdown-menu pull-right" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">Mark Resolved</a></li>
+                                    <li><a href="#">Mark Unresolve</a></li>
+                                    <!-- li class="divider"></li>
+                                    <li><a href="#">Separated link</a></li -->
                                 </ul>
                             </div>
                         </td>
